d5b1ba0b5ce5357b82b0d2d2ad12ce803fdaaba0
Haelinythii <47884588+Haelinythii@users.noreply.github.com>
2020-03-13 11:56:21 +0700

    soal 1 & 2

---
+++ soal1.c
@@ -0,0 +1,130 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int main(int argc, char** argv) {
+  #pragma region check argument error
+  for (int i = 0; i < 3; i++)
+  {
+    //check alpha?
+    for (int j = 0; j < strlen(argv[i+1]); j++)
+    {
+      if(strcmp(argv[i+1], "*") == 0)
+      {
+        continue;
+      }
+      else if(isdigit(argv[i+1][j]) == 0) //isdigit return 0 kalau bukan angka
+      {
+        //salah
+        printf("arg ke %d salah alphabet\n", i+1);
+        return 0;
+      }
+    }
+    printf("arg ke %d bener angka semua\n", i+1);
+    //check range
+    int check = atoi(argv[i+1]); //convert ke integer
+    if((i == 0 || i == 1) && (check > 59 || check < 0)) //check detik & menit
+    {
+      printf("arg ke %d salah range\n", i+1);
+      return 0;
+    }
+    if(i == 2 && (check > 23 || check < 0)) //check jam
+    {
+      printf("arg ke %d salah range\n", i+1);
+      return 0;
+    }
+    
+  }
+  if(argc != 5) //5 = program + 4 argumen lain
+  {
+    printf("salah woi kurang lol\n");
+    return 0;
+  }
+  #pragma endregion
+
+  
+  
+
+  pid_t pid, sid;
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+
+    char jamWanted[5];
+    strcpy(jamWanted, argv[3]);
+    char menitWanted[5];
+    strcpy(menitWanted, argv[2]);
+    char detikWanted[5];
+    strcpy(detikWanted, argv[1]);
+    char pathFile[1000];
+    strcpy(pathFile, argv[4]);
+
+
+  while (1) {
+    bool checkJam = false, checkMenit= false, checkDetik= false;
+    time_t curtime;
+    struct tm *loctime;
+    curtime = time (NULL);
+    loctime = localtime (&curtime);
+
+    
+    char detikNow[5], menitNow[5], jamNow[5];
+    sprintf(jamNow,"%d",loctime->tm_hour);
+    sprintf(menitNow,"%d",loctime->tm_min);
+    sprintf(detikNow,"%d", loctime->tm_sec);
+
+    //check waktunya bener ato ga
+    if(strcmp(jamWanted, "*") == 0 || strcmp(jamNow, jamWanted) == 0)
+    {
+        checkJam = true;
+    }
+    if(strcmp(menitWanted, "*") == 0 || strcmp(menitNow, menitWanted) == 0)
+    {
+        checkMenit = true;
+    }
+    if(strcmp(detikWanted, "*") == 0 || strcmp(detikNow,detikWanted) == 0)
+    {
+        checkDetik = true;
+    }
+    
+
+    if(checkJam && checkMenit && checkDetik)
+    {
+      pid_t pid;
+      pid = fork();
+        if(pid == 0)
+        {
+          char *argv[] = {"bash", pathFile, NULL};
+          execv("/bin/bash", argv);
+        }
+    }
+    sleep(1);
+  }
+}
--- soal2.c
+++ soal2.c
@@ -1,209 +1,77 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <errno.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <syslog.h>
-#include <string.h>
 #include <wait.h>
-#include <time.h>
 
-void getTimestamp(char *path);
-void writeKillerA();
-void writeKillerB();
+int main() {
+  pid_t child_id;
+  int status;
 
-int main(int argc, char **argb){
-  if(strcmp(argb[1], "-a") == 0){
-    writeKillerA();
-  }
-  if(strcmp(argb[1], "-b") == 0){
-    writeKillerB();
-  }
-
-  char buf[100];
-  char *workingDir = getcwd(buf, 100);
-
-  pid_t pid, sid;
-
-  pid = fork();
+  child_id = fork();
   
-  if (pid < 0) {
-    exit(EXIT_FAILURE);
+  if (child_id < 0) {
+    exit(EXIT_FAILURE); // Jika gagal membuat proses baru, program akan berhenti
   }
 
-  if (pid > 0) {
-    exit(EXIT_SUCCESS);
-  }
-
-  umask(0);
-
-  sid = setsid();
-  if (sid < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  if ((chdir("/")) < 0) {
-    exit(EXIT_FAILURE);
-  }
-
-  close(STDIN_FILENO);
-  close(STDOUT_FILENO);
-  close(STDERR_FILENO);
-
-  while(1){
-    chdir(workingDir);
-    pid_t child_id1;
-    int status1;
+  if (child_id == 0) {
+    // this is child
     
-    child_id1 = fork();
-
-    char namaFolder[1000];
-    snprintf(namaFolder, 1000, "./");
-    getTimestamp(namaFolder);
-
-    if(child_id1 == 0){
-      pid_t child_id2;
-      int status2;
-      
-      child_id2 = fork();
-
-      if(child_id2 == 0){
-        char *argv[] = {"mkdir", namaFolder, NULL};
-        execv("/bin/mkdir", argv);
-      } else {
-        while((wait(&status2)) > 0);
-        pid_t child_id3;
-        int status3, i;
-        char curFolder[1000]; strcpy(curFolder, namaFolder);
-        char namaFile[1000];
-        char linkDownload[1000];
-
-        for(i=0; i<20; i++){
-          strcpy(namaFile, curFolder); strcat(namaFile, "/");
-          getTimestamp(namaFile);
-          long int sec = time(NULL)%1000 + 100;
-          snprintf(linkDownload, 1000, "https://picsum.photos/%ld", sec);
-          child_id3 = fork();
-          if(child_id3 == 0){
-            char *argv[] = {"wget", "-O", namaFile, linkDownload, NULL};
-            execv("/usr/bin/wget", argv);
-          } else {
-
-          }
-          sleep(5);
-        }
-
-        pid_t child_id4;
-        int status4;
-
-        child_id4 = fork();
-
-        if(child_id4 == 0){
-          char outputZip[1000];
-          snprintf(outputZip, 1000, "%s.zip", curFolder);
-          char *argv[] = {"zip", "-r", outputZip, curFolder, NULL};
-          execv("/usr/bin/zip", argv);
-        } else {
-          while((wait(&status4)) > 0);
-          char *argv[] = {"rm", "-r", curFolder, NULL};
-          execv("/bin/rm", argv);
-        }
-      }
-    } else {
-      
+    char *argv[] = {"mkdir", "-p", "/home/dwiki/modul2/indomie", NULL};
+    execv("/bin/mkdir", argv);
+  } else {
+    // this is parent
+    while ((wait(&status)) > 0);
+    sleep(5);
+    if(fork()==0)
+    {
+      char *argv[] = {"mkdir", "-p", "/home/dwiki/modul2/sedaap", NULL};
+      execv("/bin/mkdir", argv);
     }
-    sleep(30);
-  }
+    if(fork()==0)
+    {
+      char *argv[] = {"unzip", "/home/dwiki/modul2/jpg.zip", NULL};
+      execv("/usr/bin/unzip", argv);
+    }
+    
+    else
+    {
+      while ((wait(&status)) > 0);
+      if(fork()==0)
+      {
+        char *argv[] = {"find", "/home/dwiki/modul2/jpg/.", "-maxdepth", "1", "-type", "d", "-exec", "mv", "{}", "/home/dwiki/modul2/indomie", ";", NULL};
+        execv("/usr/bin/find", argv);
+      }
+      else
+      {
+        while ((wait(&status)) > 0);
+        if(fork()==0)
+        {
+          char *argv[] = {"find", "/home/dwiki/modul2/jpg/.", "-maxdepth", "1", "-type", "f", "-exec", "mv", "{}", "/home/dwiki/modul2/sedaap", ";", NULL};
+          execv("/usr/bin/find", argv);
+        }
+        else
+        {
+          if(fork()==0)
+          {
+            char *argv[] = {"find", "/home/dwiki/modul2/indomie/.", "-mindepth", "1", "-type", "d", "-exec", "touch", "{}/coba1.txt", ";", NULL};
+            execv("/usr/bin/find", argv);
+          }
+          else
+          {
+            while ((wait(&status)) > 0);
+            sleep(3);
+            char *argv[] = {"find", "/home/dwiki/modul2/indomie/.", "-mindepth", "1", "-type", "d", "-exec", "touch", "{}/coba2.txt", ";", NULL};
+            execv("/usr/bin/find", argv);
+          }
+          
+        }
+        
 
-  return 0;
-}
-
-void getTimestamp(char *path)
-{
-  char timeStr[10];
-
-  time_t curtime;
-  struct tm *loctime;    
-  curtime = time (NULL);
-  loctime = localtime (&curtime);
-  
-  snprintf(timeStr, 10, "%d-", loctime->tm_year+1900);
-  strcat(path, timeStr);
-
-  if ((loctime->tm_mon+1) < 10)
-		snprintf(timeStr, 10, "0%d-", loctime->tm_mon+1);
-	else
-		snprintf(timeStr, 10, "%d-", loctime->tm_mon+1);
-	strcat(path, timeStr);
-	
-	if (loctime->tm_mday < 10)
-		snprintf(timeStr, 10, "0%d_", loctime->tm_mday);
-	else
-		snprintf(timeStr, 10, "%d_", loctime->tm_mday);
-	strcat(path, timeStr);
-	
-	if (loctime->tm_hour < 10)
-		snprintf(timeStr, 10, "0%d:", loctime->tm_hour);
-	else
-		snprintf(timeStr, 10, "%d:", loctime->tm_hour);
-	strcat(path, timeStr);
-	
-	if (loctime->tm_min < 10)
-		snprintf(timeStr, 10, "0%d:", loctime->tm_min);
-	else
-		snprintf(timeStr, 10, "%d:", loctime->tm_min);
-	strcat(path, timeStr);
-	
-	if (loctime->tm_sec < 10)
-		snprintf(timeStr, 10, "0%d", loctime->tm_sec);
-	else
-		snprintf(timeStr, 10, "%d", loctime->tm_sec);
-	strcat(path, timeStr);
-}
-
-void writeKillerA()
-{
-  FILE *temp;
-  temp = fopen("killer.sh", "w");
-  fputs("#!/bin/bash\n", temp);
-  fputs("killOrder=$(echo $(pidof soal2))\n", temp);
-  fputs("kill -9 $killOrder\n", temp);
-  fputs("rm $0\n", temp);
-  fclose(temp);
-
-  pid_t tempChild_id;
-  int tempStatus;
-  tempChild_id = fork();
-
-  if(tempChild_id == 0){
-    char *argv[]={"chmod", "+x", "killer.sh", NULL};
-    execv("/bin/chmod", argv);
-  } else {
-
-  }
-}
-
-void writeKillerB()
-{
-  FILE *temp;
-  temp = fopen("killer.sh", "w");
-  fputs("#!/bin/bash\n", temp);
-  fputs("killOrder=$(echo $(pidof soal2))\n", temp);
-  fputs("killOrder=${killOrder##* }\n", temp);
-  fputs("kill -9 $killOrder\n", temp);
-  fputs("rm $0\n", temp);
-  fclose(temp);
-
-  pid_t tempChild_id;
-  int tempStatus;
-  tempChild_id = fork();
-
-  if(tempChild_id == 0){
-    char *argv[]={"chmod", "+x", "killer.sh", NULL};
-    execv("/bin/chmod", argv);
-  } else {
-
+        
+      }
+    }
+    
+    
+    
   }
 }
